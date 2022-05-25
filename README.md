c01c53ef7eebcc4a64f2ccae713101b961612fd5
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-05-25 13:35:30 -0300

    blog create project

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Getting Started with Create React App
 
+Criação do projeto:
+```
+npx create-react-app blog-pessoal --template typescript
+```
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@material-ui/core';
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Hello</h1>
+            <img src="https://i.ibb.co/rdPFk39/NOGRAN-logo.png" alt="logo black" className="img"/>
+        </>
+    )
+}
+
+export default Home
