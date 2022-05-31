ad936efe58e2515acc7d66c68335041d921f0ead
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-05-31 11:42:36 -0300

    route to home from login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,4 @@
 import { Box, Button, Grid, TextField, Typography } from '@material-ui/core';
-import React from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
 
@@ -7,16 +6,16 @@ function Login() {
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
-                <Box padding={20}>
+                <Box paddingX={20}>
                     <form>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold' }}>Entrar</Typography>
-                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth/>
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth/>
+                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home'>
-                            <Button>
-                                Logar
-                            </Button>
+                            <Link to='/home' className='text-decorator-none'>
+                                <Button type='submit' variant='contained' color='primary'>
+                                    Logar
+                                </Button>
                             </Link>
                         </Box>
                     </form>
