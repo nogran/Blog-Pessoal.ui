3c467709e8f89d5d08d5a76180ee39cda6a60df5
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-01 11:38:17 -0300

    update axios and hooks

--- src/pages/cadastroUsuario/CadastroUsuario.tsx
+++ src/pages/cadastroUsuario/CadastroUsuario.tsx
@@ -1,6 +1,81 @@
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import { Box, Button, Grid, TextField, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+import User from '../../models/User';
+import { cadastroUsuario } from '../../services/Service'
 import './CadastroUsuario.css'
+
+let history = useNavigate();
+
+const [confirmarSenha, setConfirmarSenha] = useState<String>("")
+
+const [user, setUser] = useState<User>({
+    id: 0,
+    nome: "",
+    usuario: "",
+    senha: "",
+    foto: ""
+})
+
+const [userResult, setUserResult] = useState<User>({
+    id: 0,
+    nome: "",
+    usuario: "",
+    senha: "",
+    foto: ""
+})
+
+useEffect(() => {
+    if (userResult.id !== 0) {
+        history('/login')
+    }
+}, [userResult])
+
+
+function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+    setUser({
+        ...user,
+        [e.target.name]: e.target.value
+    })
+}
+
+function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>) {
+    setConfirmarSenha(e.target.value)
+}
+
+async function cadastrar(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    // Estrutura Condicional que verifica se as senhas batem e se a Senha tem mais de 8 caracteres
+    if (confirmarSenha === user.senha && user.senha.length >= 8) {
+
+        //Tenta executar o cadastro
+        try {
+            await cadastroUsuario(`/usuarios/cadastrar`, user, setUserResult)
+            alert("Usuário cadastrado com sucesso")
+
+            //Se houver erro, pegue o Erro e retorna uma msg
+        } catch (error) {
+            console.log(`Error: ${error}`)
+
+            //Pode modificar a msg de acordo com o erro 
+            alert("Usuário já existente")
+        }
+
+    } else {
+        alert("Insira no miníno 8 caracteres na senha.")
+
+        setUser({ ...user, senha: "" }) // Reinicia o campo de Senha
+        setConfirmarSenha("")           // Reinicia o campo de Confirmar Senha
+    }
+}
+
+/*
+    = : atribuição (valor = 9)
+    == : op. aritmetico (valor == 9.0)
+    === : op. idêntico (valor === 9.0)
+*/
 
 function CadastroUsuario() {
     return (
@@ -8,12 +83,16 @@ function CadastroUsuario() {
             <Grid item xs={6} className='imagem2'></Grid>
             <Grid item xs={6} alignItems='center'>
                 <Box paddingX={10}>
-                    <form>
+                    <form onSubmit={cadastrar}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold' }}>Cadastrar</Typography>
-                        <TextField id='nome' label='nome' variant='outlined' name='nome' margin='normal' fullWidth />
-                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
-                        <TextField id='confirmarSenha' label='confirmarSenha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth />
+                        <TextField onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                            value={user.nome} id='nome' label='nome' variant='outlined' name='nome' margin='normal' fullWidth required />
+                        <TextField onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                            value={user.usuario} id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth required/>
+                        <TextField onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                            value={user.usuario} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth required/>
+                        <TextField onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)}
+                            value={confirmarSenha} id='confirmarSenha' label='confirmarSenha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth required/>
                         <Box marginTop={2} textAlign='center'>
                             <Link to='/login' className='text-decorator-none'>
                                 <Button className='btnCancelar' variant='contained' color='secondary'>
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,22 +1,71 @@
+import React, { useState, ChangeEvent, useEffect } from 'react';
 import { Box, Button, Grid, TextField, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { login } from '../../services/Service';
+import UserLogin from '../../models/UserLogin';
 import './Login.css';
 
 function Login() {
+
+    // Redireciona o usuário para determinada pagina
+    let history = useNavigate();
+
+    // Hooks que vão manipular o nosso Local Storage para gravar o Token
+    const [token, setToken] = useLocalStorage('token')
+
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: '',
+            foto: '',
+            token: ''
+        }
+    )
+    // Atualiza a model com o valor que o usurio digitar no campo de input.
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    useEffect(() => {
+        if (token !== "") {
+            history('/home')
+        }
+    }, [token])
+
+    // Envia os dados da requisição (dados de Login).
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+
+        // Impede o comportamento padrão do botão (de atualizar a tela).
+        e.preventDefault();
+
+        try {
+            await login(`/usuarios/logar`, userLogin, setToken)
+
+            alert('Usuario logado com sucesso');
+        } catch (error) {
+            alert('Dados do usuário incosistentes. Erro ao logar');
+        }
+    }
+
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold' }}>Entrar</Typography>
-                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
-                                <Button type='submit' variant='contained' color='primary'>
-                                    Logar
-                                </Button>
-                            </Link>
+                            <Button type='submit' variant='contained' color='primary'>
+                                Logar
+                            </Button>
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
