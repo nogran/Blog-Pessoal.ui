8acd522ae53f7eabb34c7baf4b106c5108939497
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-06 14:10:07 -0300

    lista postagens

--- src/components/postagens/listapostagem/ListaPostagem.tsx
+++ src/components/postagens/listapostagem/ListaPostagem.tsx
@@ -1,48 +1,80 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import './ListaPostagem.css';
+import useLocalStorage from 'react-use-localstorage';
+import { useEffect, useState } from 'react';
+import Postagem from '../../../models/Postagem';
+import { busca } from '../../../services/Service';
 
 function ListaPostagem() {
 
+  const [posts, setPosts] = useState<Postagem[]>([])
+  const [token, setToken] = useLocalStorage('token');
+  let history = useNavigate();
+
+  useEffect(() => {
+    if (token === '') {
+      alert("Voce precisa estar logado.")
+      history("/login")
+    }
+  }, [token])
+
+  async function getPost() {
+    await busca("/Tema", setPosts, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
+
+  useEffect(() => {
+    getPost()
+  }, [posts.length])
+
+
   return (
     <>
-      <Box m={2} >
-        <Card variant="outlined">
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Postagens
-            </Typography>
-            <Typography variant="h5" component="h2">
-              Título
-            </Typography>
-            <Typography variant="body2" component="p">
-              Texto da Postagem
-            </Typography>
-            <Typography variant="body2" component="p">
-              Tema
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Box display="flex" justifyContent="center" mb={1.5}>
+      {
+        posts.map(post => (
+          <Box m={2} >
+            <Card variant="outlined">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  Postagens
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {post.titulo}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {post.texto}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {post.tema?.descricao}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box display="flex" justifyContent="center" mb={1.5}>
 
-              <Link to="" className="text-decorator-none" >
-                <Box mx={1}>
-                  <Button variant="contained" className="marginLeft" size='small' color="primary" >
-                    atualizar
-                  </Button>
+                  <Link to={`/formularioPostagem/${post.id}`} className="text-decorator-none" >
+                    <Box mx={1}>
+                      <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                        atualizar
+                      </Button>
+                    </Box>
+                  </Link>
+                  <Link to={`/deletarPostagem/${post.id}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" size='small' color="secondary">
+                        deletar
+                      </Button>
+                    </Box>
+                  </Link>
                 </Box>
-              </Link>
-              <Link to="" className="text-decorator-none">
-                <Box mx={1}>
-                  <Button variant="contained" size='small' color="secondary">
-                    deletar
-                  </Button>
-                </Box>
-              </Link>
-            </Box>
-          </CardActions>
-        </Card>
-      </Box>
+              </CardActions>
+            </Card>
+          </Box>
+        ))
+      }
     </>)
 }
 
