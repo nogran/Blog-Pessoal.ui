cf97c86612e93ed445f0c80ed2caed476d960b27
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-06 14:02:17 -0300

    update Tema

--- src/components/temas/listatema/ListaTema.tsx
+++ src/components/temas/listatema/ListaTema.tsx
@@ -1,11 +1,40 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import useLocalStorage from 'react-use-localstorage';
+import { busca } from '../../../services/Service';
+import Tema from '../../../models/Tema';
 import './ListaTema.css';
 
 function ListaTema() {
 
+  const [temas, setTemas] = useState<Tema[]>([])
+  const [token, setToken] = useLocalStorage('token');
+  let history = useNavigate();
+
+  useEffect(() => {
+    if (token === '') {
+      alert("Voce precisa estar logado.")
+      history("/login")
+    }
+  }, [token])
+
+  async function getTema() {
+    await busca("/Tema", setTemas, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
+
+  useEffect(() => {
+    getTema()
+  }, [temas.length])
+
   return (
     <>
+    {
+      temas.map(tema => (
       <Box m={2} >
         <Card variant="outlined">
           <CardContent>
@@ -13,20 +42,20 @@ function ListaTema() {
               Tema
             </Typography>
             <Typography variant="h5" component="h2">
-              Minha descrição
+              {tema.descricao}
             </Typography>
           </CardContent>
           <CardActions>
             <Box display="flex" justifyContent="center" mb={1.5} >
 
-              <Link to="" className="text-decorator-none">
+              <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
                 <Box mx={1}>
                   <Button variant="contained" className="marginLeft" size='small' color="primary" >
                     atualizar
                   </Button>
                 </Box>
               </Link>
-              <Link to="" className="text-decorator-none">
+              <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
                 <Box mx={1}>
                   <Button variant="contained" size='small' color="secondary">
                     deletar
@@ -37,6 +66,8 @@ function ListaTema() {
           </CardActions>
         </Card>
       </Box>
+      ))
+    }
     </>
   );
 }
