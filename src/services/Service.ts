98ff56445504f2c0eec31959304756364177c60a
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-07 23:41:35 -0300

    create buscaId, post,  put and deleteId

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,7 +4,7 @@ export const api = axios.create({
     baseURL: 'https://nogran-blog.herokuapp.com'
 })
 
-export const cadastroUsuario = async(url: any, dados: any, setDados: any) => {
+export const cadastroUsuario = async (url: any, dados: any, setDados: any) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
 }
@@ -14,7 +14,27 @@ export const login = async (url: any, dados: any, setDado: any) => {
     setDado(resposta.data.token)
 }
 
-export const busca = async(url: any, setDado: any, header: any) => { 
+export const busca = async (url: any, setDado: any, header: any) => {
     const resposta = await api.get(url, header)
     setDado(resposta.data)
 }
+
+export const buscaId = async (url: any, setDado: any, header: any) => {
+    const resposta = await api.get(url, header)
+    setDado(resposta.data)
+}
+
+export const post = async (url: any, dados: any, setDado: any, header: any) => {
+    const resposta = await api.post(url, dados, header)
+    setDado(resposta.data)
+}
+
+export const put = async (url: any, dados: any, setDado: any, header: any) => {
+    const resposta = await api.put(url, dados, header)
+    setDado(resposta.data)
+}
+
+export const deleteId = async (url: any, header: any) => {
+    await api.delete(url, header)
+}
+
