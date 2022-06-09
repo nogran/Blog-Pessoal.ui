7943e7ed015fac06b56ef2f2fbbfaff290f6908b
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-09 20:31:26 -0300

    toast lista postagem update

--- src/components/postagens/listapostagem/ListaPostagem.tsx
+++ src/components/postagens/listapostagem/ListaPostagem.tsx
@@ -1,26 +1,42 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import './ListaPostagem.css';
-import useLocalStorage from 'react-use-localstorage';
+// import useLocalStorage from 'react-use-localstorage';
 import { useEffect, useState } from 'react';
 import Postagem from '../../../models/Postagem';
 import { busca } from '../../../services/Service';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { UserState } from '../../../store/tokens/useReducer';
 
 function ListaPostagem() {
 
   const [posts, setPosts] = useState<Postagem[]>([])
-  const [token, setToken] = useLocalStorage('token');
+  // const [token, setToken] = useLocalStorage('token');
+  const token = useSelector<UserState, UserState["tokens"]>(
+    (state) => state.tokens
+  );
   let history = useNavigate();
 
   useEffect(() => {
     if (token === '') {
-      alert("Voce precisa estar logado.")
+      // alert("Voce precisa estar logado.")
+      toast.error('Você precisa estar logado!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark"
+      });
       history("/login")
     }
   }, [token])
 
   async function getPost() {
-    await busca("/Tema", setPosts, {
+    await busca("/postagens", setPosts, {
       headers: {
         'Authorization': token
       }
